155324d486e2c77350d8d3ed603421231b9f7b4f
Aniu <yinjiazeng@163.com>
2020-04-19 18:40:43 +0800

    commit

--- packages/docfree-utils/src/babelOptions.ts
+++ packages/docfree-utils/src/babelOptions.ts
@@ -1,6 +1,13 @@
 export default {
   presets: [
-    require.resolve('@babel/preset-env'),
+    [
+      require.resolve('@babel/preset-env'),
+      {
+        targets: {
+          ie: '9',
+        },
+      },
+    ],
     require.resolve('@babel/preset-react'),
     require.resolve('@babel/preset-typescript'),
   ],
