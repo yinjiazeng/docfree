b972831a691fe84393c98bc78888fcfd64e2f590
Aniu <yinjiazeng@163.com>
2022-02-09 10:17:02 +0800

    commit

--- packages/docfree-generate/src/generateBlogRoutes.ts
+++ packages/docfree-generate/src/generateBlogRoutes.ts
@@ -1,18 +1,15 @@
 import { RouteItem } from './generateData';
 
 export default function generateBlogRoutes(routes: RouteItem[] = [], pathname = '/'): RouteItem[] {
-  routes.forEach((data) => {
-    const { children, path } = data;
-
-    if (Array.isArray(children)) {
-      generateBlogRoutes(children, `${path}/`);
-    }
-  });
-
   for (const route of routes) {
-    const { filename, path }: any = route;
+    const { children, path, filename }: any = route;
+
     if (path === '/' && /^README$/i.test(filename)) {
       return routes;
+    }
+
+    if (Array.isArray(children)) {
+      generateBlogRoutes(children, `${pathname}/${path}/`.replace(/\/+/g, '/'));
     }
   }
 
